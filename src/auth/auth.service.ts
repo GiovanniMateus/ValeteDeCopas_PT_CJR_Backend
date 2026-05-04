a57870b0a15814d0609f89ecbf9bbf8a63da0e26
Thiago Henrique <estudos.trabalhos12@gmail.com>
2026-05-04 11:29:52 -0300

    feat: adiciona auth controller e service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../database/prisma.service';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: loginDto.email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Email ou senha inválidos');
+    }
+
+    const senhaCorreta = await bcrypt.compare(loginDto.senha, user.senha_hash);
+
+    if (!senhaCorreta) {
+      throw new UnauthorizedException('Email ou senha inválidos');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+
+    const token = this.jwtService.sign(payload);
+
+    return {
+      access_token: token,
+      user: {
+        id: user.id,
+        nome: user.nome,
+        email: user.email,
+        username: user.username,
+      },
+    };
+  }
+}
