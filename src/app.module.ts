c7ac2b13cf5f555a5825d6a68a82b6c3629bc448
RodrigoDutraF88 <rodrigodutraf88@gmail.com>
2026-05-16 15:56:33 -0300

    feat: adiciona crud de categorias e seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ProdutosModule } from './produtos/produtos.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ProdutosModule } from './produtos/produtos.module';
     UsersModule,
     AuthModule,
     ProdutosModule,
+    CategoriasModule,
   ],
   controllers: [AppController],
   providers: [AppService],
