146c96feb04172ad367a80b66fc067e687c4f1f4
Thiago Henrique <estudos.trabalhos12@gmail.com>
2026-05-23 00:37:20 -0300

    feat: implementa salvamento de fotos para criação de loja

--- src/lojas/lojas.controller.ts
+++ src/lojas/lojas.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete, UseInterceptors, 
+  UploadedFiles, BadRequestException, UseGuards, Req,
+  UnauthorizedException} from '@nestjs/common';
 import { CreateLojasDto } from '../lojas/dto/create-lojas.dto';
 import { UpdateLojaDto } from './dto/update-lojas.dto';
 import { LojasService } from './lojas.service';
+import {diskStorage} from 'multer';
+import { extname } from 'path';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import type { Request } from 'express';
 
 @Controller('lojas')
 export class LojasController {
@@ -11,9 +18,65 @@ export class LojasController {
   ) {}
 
   @Post()
-  async create(@Body() data: CreateLojasDto) {
-    return this.lojasService.create(data);
+  @UseGuards(JwtAuthGuard) 
+  @UseInterceptors(FileFieldsInterceptor([
+   
+    { name: 'logo', maxCount: 1 },
+    { name: 'banner', maxCount: 1 },
+    { name: 'sticker', maxCount: 1 },
+  ], {
+    storage: diskStorage({
+      destination: './uploads/lojas', //esse é o destino das  imagens 
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, `${file.fieldname}-${uniqueSuffix}${extname(file.originalname)}`);
+      }
+    })
+  }))
+
+  async create(
+    @Req() req: Request,
+    @Body() body :any,
+    @UploadedFiles() files: { 
+      logo?: Express.Multer.File[], 
+      banner?: Express.Multer.File[], 
+      sticker?: Express.Multer.File[] 
+    }
+  ) {
+
+    const userId =(req as any).user?.id; 
+    
+    if(!userId){
+      throw new UnauthorizedException("Usuário não está autenticado. Token ausente ou inválido") 
+    }
+
+    if (!body.categoriaId || !body.nome) {
+      throw new BadRequestException('Campos obrigatórios ausentes: categoriaId ou nome');
+    }
+
+    
+    const lojaData: Partial<CreateLojasDto> = {
+      userId: userId,
+      categoriaId: Number(body.categoriaId),
+      nome: body.nome,
+      descricao: body.descricao || '', //estou passando descrição para caso futuramente esse campo venha existir
+    };
+
+    
+    if (files?.logo?.[0]) {
+      lojaData.logoUrl = `/uploads/lojas/${files.logo[0].filename}`;
+    }
+    if (files?.banner?.[0]) {
+      lojaData.bannerUrl = `/uploads/lojas/${files.banner[0].filename}`;
+    }
+    if (files?.sticker?.[0]) {
+      lojaData.stickerUrl = `/uploads/lojas/${files.sticker[0].filename}`;
+    }
+
+
+    return this.lojasService.create(lojaData as CreateLojasDto);
   }
+
 
   @Get()
   async findAll() {
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
   app.enableCors({
     origin: 'http://localhost:3000', // porta do Frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // métodos permitidos
     
   });
+
+
+  // a pasta uploads vai ficar aberta para que seus arquivos possam ser acessados
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/', 
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
